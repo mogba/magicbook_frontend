e205a210525935ee51fbdfac785ff079821f738b
Gabriel de Morais <gabrieldemorais6@gmail.com>
2024-10-02 19:30:40 -0300

    feat: add crud hook for youtube urls

--- app/ui/api/use-youtube-urls.ts
+++ app/ui/api/use-youtube-urls.ts
@@ -1,17 +1,65 @@
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
 import { useMagicFetch } from "./use-magic-fetch";
 
-export function useGetYouTubeUrls() {
+export function useYoutubeUrls() {
+  const queryClient = useQueryClient();
   const { magicFetch } = useMagicFetch();
 
-  return useQuery({
-    queryKey: ["content"],
-    queryFn: async () => {
-      const response = await magicFetch<
-        { id: number; url: string; video_id: string; date_created: string }[]
-      >("youtube-urls");
-      return response.data;
-    },
-  });
+  function useGetYouTubeUrls() {
+    return useQuery({
+      queryKey: ["videos"],
+      queryFn: async () =>
+        await magicFetch<
+          { id: number; url: string; video_id: string; date_created: string }[]
+        >("youtube-urls/"),
+    });
+  }
+
+  function invalidateVideosQuery() {
+    queryClient.invalidateQueries("videos");
+  }
+
+  function createYouTubeUrl(url: string) {
+    magicFetch<void>("youtube-urls/", {
+      method: "POST",
+      body: { url },
+    });
+
+    invalidateVideosQuery();
+  }
+
+  function getYouTubeUrl(id: number) {
+    return magicFetch<{
+      id: number;
+      url: string;
+      video_id: string;
+      date_created: string;
+    }>(`youtube-urls/${id}/`);
+  }
+
+  function updateYouTubeUrl(id: number, url: string) {
+    magicFetch<void>(`youtube-urls/${id}/`, {
+      method: "PUT",
+      body: { url },
+    });
+
+    invalidateVideosQuery();
+  }
+
+  function deleteYouTubeUrl(id: number) {
+    magicFetch<void>(`youtube-urls/${id}/`, {
+      method: "DELETE",
+    });
+
+    invalidateVideosQuery();
+  }
+
+  return {
+    useGetYouTubeUrls,
+    createYouTubeUrl,
+    getYouTubeUrl,
+    updateYouTubeUrl,
+    deleteYouTubeUrl,
+  };
 }
